a415bc3ba0b860681399bea71d39e1cdc31f8e50
Vilayat <vilayatk96@outlook.com>
2024-03-09 02:15:35 +0530

    Implement Models and Controllers

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] // Attribute
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "I'm OK";
+        }
+    }
+}
---
+++ Models/ShopContext.cs
@@ -0,0 +1,25 @@
+﻿using MyFirstAPI.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MyFirstAPI.Models
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(a => a.Category)
+                .HasForeignKey(a => a.CategoryId);
+
+            modelBuilder.Seed();
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
