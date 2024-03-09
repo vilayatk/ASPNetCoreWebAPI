ca686682ffa4376678987d5ac7a25b0d1f1b1739
Vilayat <vilayatk96@outlook.com>
2024-03-09 12:43:49 +0530

    Implement a HTTP POST Method and Return the added product by id using existing Get Function

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -22,7 +22,12 @@ namespace MyFirstAPI.Controllers
             return Ok(_context.Products.ToArray()); // Status Code 200 OK
         }
 
-        //[HttpGet, Route("api/Products/{id}")] // OR  Route("{id}") OR [HttpGet("{id}")]
+        /*
+            [HttpGet, Route("api/Products/{id}")] // OR  Route("{id}") OR [HttpGet("{id}")]
+            The int argument comes[FromRoute] -> WebAPI is clever enough to get from Route itself
+            we can even get it[FromBody], Request Body
+            [FromQuery] -> Data from URL
+        */
         //public ActionResult GetProduct(int id) // id is passed from {id} parameter from the Route
         //{
         //    var product = _context.Products.Find(id);
@@ -31,8 +36,8 @@ namespace MyFirstAPI.Controllers
         //    // Ok : Return product with Product
         //}
 
-        [HttpGet, Route("api/Products/{id}")] // OR  Route("{id}") OR [HttpGet("{id}")]
-        public async Task<ActionResult> GetProduct(int id) // id is passed from {id} parameter from the Route
+        [HttpGet, Route("api/Products/{id}")]
+        public async Task<ActionResult> GetProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
             return product is null ? NotFound("Product with id: " + id + " does not exist") : Ok(product);
@@ -40,5 +45,14 @@ namespace MyFirstAPI.Controllers
             // Ok : Return product with Product
         }
 
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product) // Interface to return Custom Action Results
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            // Status 201 Created! // We call the GetProduct Method to return the product by id
+            return CreatedAtAction("GetProduct",
+                new { id = product.Id }, product);
+        }
     }
 }
--- Models/Products.cs
+++ Models/Products.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace MyFirstAPI.Models
 {
@@ -10,9 +11,10 @@ namespace MyFirstAPI.Models
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
+        [Required]
         public int CategoryId { get; set; }
         [JsonIgnore]
-        public virtual Category Category { get; set; }
+        public virtual Category? Category { get; set; }
 
     }
 }
